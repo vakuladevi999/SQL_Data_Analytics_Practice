74ec473ccfd288bac3630fbf05e937def748eb04
vakuladevi999 <vakuladevi2006@gmail.com>
2026-09-21 08:38:26 +0530

    Add files via upload

---
+++ Day 12/Problem 3.sql
@@ -0,0 +1,35 @@
+-- Q:- Find customers who made purchases in two consecutive months.
+
+-- Creating Orders table
+CREATE TABLE Orders
+(OrderID INT,CustomerID,Product VARCHAR(20),
+Category VARCHAR(20),Amount INT,OrderDate)
+
+-- Inserting values into Orders table
+INSERT INTO Orders VALUES
+(101,1,'Laptop','Electronics',60000.00,'2024-01-10'),
+(102,2,'Mobile','Electronics',25000.00,'2024-01-15'),
+(103,1,'Mouse','Accessories',700.00,'2024-02-01'),
+(104,3,'Laptop','Electronics',62000.00,'2024-02-10'),
+(105,5,'Keyboard','Accessories',1500.00,'2024-03-05'),
+(106,7,'Monitor','Electronics',12000.00,'2024-03-10'),
+(107,2,'Headphones','Accessories',3000.00,'2024-04-02'),
+(108,6,'Laptop','Electronics',58000.00,'2024-04-15'),
+(109,7,'Mouse','Accessories',800.00,'2024-04-20'),
+(110,5,'Mobile','Electronics',28000.00,'2024-05-01');
+
+-- Ans:-
+
+WITH CTE AS (
+SELECT
+	CustomerID,
+	OrderDate,
+	LAG(OrderDate) OVER(PARTITION BY CustomerID ORDER BY OrderDate) AS PreviousOrderDate
+FROM Orders )
+
+	SELECT
+		CustomerID,
+		COUNT(*)
+	FROM CTE
+	WHERE DATEDIFF(MONTH,PreviousOrderDate,OrderDate) = 1
+	GROUP BY CustomerID;
